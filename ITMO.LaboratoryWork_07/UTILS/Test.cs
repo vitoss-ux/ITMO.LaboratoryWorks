0111b8335b5c8f2f08e404c1fd51a89308c4a3c8
vitoss-ux <73466603+vitoss-ux@users.noreply.github.com>
2020-11-20 21:36:13 +0300

    Add files via upload

---
+++ ITMO.LaboratoryWork_07/UTILS/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Utils
+{
+    class Test
+    {
+        public static void Main()
+        {
+            string message;
+
+            // Get an input string
+            Console.WriteLine("Enter string to reverse:");
+            message = Console.ReadLine();
+
+            // Reverse the string
+            Utils.Reverse(ref message);
+
+            // Display the result
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+    }
+}
